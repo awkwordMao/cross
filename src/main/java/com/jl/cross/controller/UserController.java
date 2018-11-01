4a3944fe6a7213141d1a856f7a39e2de56067e33
李杰 <2624824150@qq.com>
2018-11-01 17:44:02 +0800

    MD5加密注册登录

--- src/main/java/com/jl/cross/controller/UserController.java
+++ src/main/java/com/jl/cross/controller/UserController.java
@@ -2,16 +2,20 @@ package com.jl.cross.controller;
 
 import com.jl.cross.model.User;
 import com.jl.cross.service.UserService;
+import com.jl.cross.util.MD5Util;
 import com.jl.cross.util.Result;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import sun.plugin2.message.Message;
 
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 import javax.websocket.server.PathParam;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 @Controller
 @RequestMapping("/user")
@@ -22,11 +26,13 @@ public class UserController {
 
     @ResponseBody
     @RequestMapping(value = "/regist", method = RequestMethod.POST)
-    public Result<User> regist(@PathParam("name") String name, @PathParam("pssword") String password){
+    public Result<User> regist(@PathParam("name") String name, @PathParam("pssword") String password) throws NoSuchAlgorithmException {
+        String mdPassword = MD5Util.encodeByMD5(password);
+        System.out.println(mdPassword);
         Result<User> result = new Result<User>();
         User user = new User();
         user.setName(name);
-        user.setPassword(password);
+        user.setPassword(mdPassword);
         result = userService.regist(user);
         return result;
     }
@@ -35,7 +41,8 @@ public class UserController {
     @RequestMapping(value = "/login", method = RequestMethod.POST)
     public Result<User> login(@PathParam("name") String name, @PathParam("password") String password, HttpServletRequest request){
 //        System.out.println(name + "=======" + password);
-        Result<User> result = userService.login(name, password, request);
+        String mdPassword = MD5Util.encodeByMD5(password);
+        Result<User> result = userService.login(name, mdPassword, request);
         return result;
     }
 
